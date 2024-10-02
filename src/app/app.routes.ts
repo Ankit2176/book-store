e8e87bf12b8cb15747623c83c482625dbfcf5c9d
FT07\Admin <yash.facile@outlook.com>
2024-10-02 12:40:01 +0530

    Layout change and routing added yash2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { BlogLayoutComponent } from './blogs/blog-layout/blog-layout.component';
 import { SinglePageLayoutComponent } from './singlePage/single-page-layout/single-page-layout.component';
+import { ShopLayoutComponent } from './shop/shop-layout/shop-layout.component';
+import { CartLayoutComponent } from './cart/cart-layout/cart-layout.component';
+import { CheckoutLayoutComponent } from './checkout/checkout-layout/checkout-layout.component';
+import { SinglePostLayoutComponent } from './single-post/single-post-layout/single-post-layout.component';
+import { ContactLayoutComponent } from './contact/contact-layout/contact-layout.component';
+import { HomeLayoutComponent } from './home/home-layout/home-layout.component';
 
 export const routes: Routes = [
 
@@ -10,12 +16,36 @@ export const routes: Routes = [
     component:LayoutComponent,
     children:[
       {
+        path:'home',
+        component:HomeLayoutComponent
+      },
+      {
         path:'blog',
         component:BlogLayoutComponent
       },
       {
         path:'singlePage',
         component:SinglePageLayoutComponent
+      },
+      {
+        path:'cart',
+        component:CartLayoutComponent
+      },
+      {
+        path:'checkout',
+        component:CheckoutLayoutComponent
+      },
+      {
+        path:'singlePost',
+        component:SinglePostLayoutComponent
+      },
+      {
+        path:'contact',
+        component:ContactLayoutComponent
+      },
+      {
+        path:'shop',
+        component:ShopLayoutComponent
       }
     ]
   }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -7,11 +7,12 @@ import { LatestPostsComponent } from '../commanLayout/latest-posts/latest-posts.
 import { InstagramComponent } from '../commanLayout/instagram/instagram.component';
 import { FooterComponent } from '../commanLayout/footer/footer.component';
 import { FooterBottomComponent } from '../commanLayout/footer-bottom/footer-bottom.component';
+import { HomeLayoutComponent } from "../home/home-layout/home-layout.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [ RouterOutlet,RouterLink,SearchPopupComponent, HeaderComponent, CustomerReviewsComponent, LatestPostsComponent, InstagramComponent, FooterComponent, FooterBottomComponent],
+  imports: [RouterOutlet, RouterLink, SearchPopupComponent, HeaderComponent, CustomerReviewsComponent, LatestPostsComponent, InstagramComponent, FooterComponent, FooterBottomComponent, HomeLayoutComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
